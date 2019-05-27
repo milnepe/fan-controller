a6da0e68c4ab2acf8c86ff263811239c813cbff2
Peter Milne <peter@ursaleo.com>
2019-05-27 18:42:11 +0100

    add software serial

--- FanControllerTest/FanControllerTest.ino
+++ FanControllerTest/FanControllerTest.ino
@@ -1,28 +1,32 @@
 /*
   Radial-G fan controller output test
+ 
+ Author: Pete Milne
+ Date: 27-05-2019
+ Version: 0.1
+ */
 
-  Author: Pete Milne
-  Date: 27-05-2019
-  Version: 0.1
-*/
+#include <SoftwareSerial.h>
 
-long RGspeed; // Speed in kph sent from Radial-G
+SoftwareSerial serial1(2, 3); // RX, TX
+
+long output; // serial output
 
 void setup() {
   Serial.begin(9600);
+  serial1.begin(9600);
 }
 
 void loop() {
 
   if (Serial.available() > 0) { // Read buffer
-    RGspeed = Serial.parseInt(); // Convert digits until newline
+    output = Serial.parseInt(); // Convert digits until newline
   }
 
   //print the results to the Serial Monitor:
-  Serial.print("RG Speed = ");
-  Serial.print(RGspeed);
-  Serial.println("kph");
+  serial1.println(output);
   delay(2);
 }
+
 
 
